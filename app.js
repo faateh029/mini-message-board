363438d5eddfad8ab3704542ab15dbeecc38bae9
faateh029 <buttfatih691@gmail.com>
2025-07-28 18:22:39 +0500

    installed pg and setup pool

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import messageRouter from './routes/messageRoute.js';
+import fs from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import fs from 'fs/promises';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const PORT = process.env.PORT || 3000;
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 
 app.use(express.static(path.join(__dirname, "Public")));
---
+++ db/pool.js
@@ -0,0 +1,11 @@
+import {pool} from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const pool = new Pool({
+   host:process.env.DB_HOST,  
+    user: process.env.DB_USER ,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DATABASE,
+    port:process.env.DB_PORT
+})
